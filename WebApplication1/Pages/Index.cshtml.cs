aeebe42052bf1a76fa7a69154e0231ff2146e71e
amititconsultant@hotmail.com <amititconsultant@hotmail.com>
2018-04-30 13:23:47 -0500

    first commit

---
+++ WebApplication1/Pages/Index.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+
+using AKS_API.Model;
+
+namespace WebApplication1.Pages
+{
+    public class IndexModel : PageModel
+    {
+
+        public List<Hero> Heroes { get; set; }
+
+        public void OnGet()
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://172.20.0.4:82/");
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            HttpResponseMessage message = client.GetAsync("api/heroes").Result;
+            if (message.IsSuccessStatusCode)
+            {
+                string result = message.Content.ReadAsStringAsync().Result;
+                Heroes = JsonConvert.DeserializeObject<List<Hero>>(result);
+            }
+        }
+    }
+}
